8aaa952085d4536c75f4e278194422be2d86a5cb
Dat T. Tran <datttrian@gmail.com>
2023-07-08 09:49:03 -0400

    Add Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,31 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <ClientOnly>
+          <Modal />
+          <Navbar />
+          {children}
+        </ClientOnly>
       </body>
     </html>
-  )
+  );
 }
